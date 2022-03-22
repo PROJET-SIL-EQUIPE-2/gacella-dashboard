d1684282135d7df445a70e8c9d90577e76791dfd
namgalff <96527743+namgalff@users.noreply.github.com>
2022-03-22 14:09:34 +0100

    valider/refuser tests

--- src/__test__/login.test.js
+++ src/__test__/login.test.js
@@ -1,31 +1,26 @@
-import { render, screen, cleanup} from '@testing-library/react';
-import Login from './function';  //must changing
-
-
-
+import { render, screen, cleanup } from "@testing-library/react";
+import Login from "./login"; //must changing
 
 //test success
-it('Login success',  async() => {
-    let username="iz_mahdi@esi.dz"
-    let password= "test"
-       //Appel la fonction login
-       await Login(username,password).expect(200);// cette ligne à remplacer
-      // ça depend le traitement de la fonction 
-   } )
+it("Login success", async () => {
+  let username = "iz_mahdi@esi.dz";
+  let password = "test";
+  //Appel la fonction login
+  await Login(username, password).expect(200); // cette ligne à remplacer
+  // ça depend le traitement de la fonction
+});
 
+//test email n'existe pas
+it("fails when an email does not exist supplied", async () => {
+  let mail = "test@test.test";
+  let mdp = "test";
+  //Appel la fonction login
+  await Login(username, password).expect(400); //cette fonction àremplacer
+});
 
- //test email n'existe pas
- it('fails when an email does not exist supplied', async() => {
-    let mail ="test@test.test"
-    let mdp ="test"
-    //Appel la fonction login
-   await Login(username,password).expect(400); //cette fonction àremplacer
-} )
- 
- //test  empty or incorrect email and password
- it('fails when an incorrect/empty email and password is supplied', async () => {
-  
-    let mail= 'test123@test.com'
-    let mdp= 'incorrectpassword'
-     await login(mail,mdp).expect(400)
-  })
+//test  empty or incorrect email and password
+it("fails when an incorrect/empty email and password is supplied", async () => {
+  let mail = "test123@test.com";
+  let mdp = "incorrectpassword";
+  await login(mail, mdp).expect(400);
+});
---
+++ src/__test__/val_rej.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function validerUtil(util) {}
+
+function refuserUtil(util) {}
+
+export { validerUtil, refuserUtil };

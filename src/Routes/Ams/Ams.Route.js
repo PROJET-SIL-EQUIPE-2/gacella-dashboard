4b805ce38028f24fad4936e4b14f668b0e5cc869
namgalff <96527743+namgalff@users.noreply.github.com>
2022-04-09 14:42:47 +0200

    profile/véhicules

--- src/Routes/Ams/Ams.Route.js
+++ src/Routes/Ams/Ams.Route.js
@@ -1,124 +1,163 @@
-import * as React from 'react';
-import MaterialTable from 'material-table';
-import {tableLang , tableIcons} from '../../ui-component/ReactTablesWidget/Widget'
-import profileIcon from "./assets/ProfileIcon.png"
+import * as React from "react";
+import MaterialTable from "material-table";
+import {
+  tableLang,
+  tableIcons,
+} from "../../ui-component/ReactTablesWidget/Widget";
+import profileIcon from "./assets/ProfileIcon.png";
 import RemoveIcon from "./assets/RemoveIcon.png";
 import AddIcon from "./assets/AddIcon.png";
-import './styles.css';
+import "./styles.css";
 import ProfileDialog from "./Compounents/ProfileDialog.Compounent";
 import ConfirmDialog from "./Compounents/ConfirmDialog.Compounent";
 import SignUpDialog from "./Compounents/SignUpDialog.Compounent";
-import {useState , useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchgetAMS,fetchgetAM} from "../../redux/actions/actions";
-
-
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchgetAMS, fetchgetAM } from "../../redux/actions/actions";
 
 export default function AmsRoute() {
-    const [isAmProfileDialogopen, setAmProfileDialogOpenStatus] = useState(false);
-    const [isConfirmDialogopen, setConfirmDialogOpenStatus] = useState(false);
-    const [isSignUpDialogopen, setSignUpDialogOpenStatus] = useState(false);
-    const [amId, setAmId] = useState(null);
-    const [amEmail, setAmEmail] = useState(null);
-    const dispatch = useDispatch();
-    const am = useSelector(state => state.amsProfiles);
+  const [isAmProfileDialogopen, setAmProfileDialogOpenStatus] = useState(false);
+  const [isConfirmDialogopen, setConfirmDialogOpenStatus] = useState(false);
+  const [isSignUpDialogopen, setSignUpDialogOpenStatus] = useState(false);
+  const [amId, setAmId] = useState(null);
+  const [amEmail, setAmEmail] = useState(null);
+  const dispatch = useDispatch();
+  const am = useSelector((state) => state.amsProfiles);
 
-    const  baseUrlTest = "http://localhost:3000";
+  const baseUrlTest = "http://localhost:3000";
 
-    useEffect(()=>{
-        dispatch(fetchgetAMS());
-    }, [])
-    
-    const columns=[
-        { 
-            title: 'Nom Complet',
-            field: 'name_familyName',
-            render: rowData =>(<div className="roboto-500">{rowData.familyName} {rowData.name}</div>)
-        },
-        {
-            title: 'Adresse Email',
-            field: 'email',
-            render: rowData =>(<div className="roboto-500">{rowData.email}</div>)
-        },
-        {
-            title: 'Numéro de téléphone',
-            field: 'phoneNumber',
-            render: rowData =>(<div className="roboto-500">{rowData.phone_number}</div>)
-        },
-        {
-            title: 'Profile',
-            field: 'profile',
-            render: rowData =>(<img className="hoverable"
-                                    onClick={() =>{
-                                            /**/
-                                            setAmProfileDialogOpenStatus(true); 
-                                            dispatch(fetchgetAM(rowData.agent_id));
-                                        }}
-                                    style={{ height : "40px" , width : "40px" }}
-                                    src={profileIcon}
-                                />)
-        },
-        {
-            title: 'Delete',
-            field: 'Delete',
-            render: rowData =>(<img className="hoverable"
-                                    onClick={() =>{
-                                            /**/
-                                            setConfirmDialogOpenStatus(true);
-                                            setAmEmail(rowData.email);
-                                        }}
-                                    style={{ height : "40px" , width : "40px" }}
-                                    src={RemoveIcon}
-                                />)
-        },
-    ] 
+  useEffect(() => {
+    dispatch(fetchgetAMS());
+  }, []);
 
-    const actions=[
-        {
-             icon:  ()=>(<img style={{ height : "30px" , width : "30px" }}  src={AddIcon}/>),
-             tooltip: 'Ajouter un agent',
-             position:'toolbar',
-             isFreeAction: true,
-            onClick: (event) => {setSignUpDialogOpenStatus(true); },
-        }
-     ]
-    
-    return (
-        (am.loading || am.error) ? null : (
-                <div className="bg-white" style={{ height: 400, width: '100%' }}>
-                    <MaterialTable
-                        style={{borderRadius : '25px'}}
-                        icons={tableIcons}
-                        localization={tableLang}
-                        title="Gestions des comptes des agents de maintenance"
-                        columns={columns}
-                        data={am.data.allAgents}
-                        actions={actions}
-                        options={{
-                            search:false,
-                            selection:true,
-                            sorting:true,
-                            //filtering:true,
-                            showSelectAllCheckbox:true,
-                            showTextRowsSelected:true,
-                            actionsColumnIndex: -1,
-                            headerStyle: {
-                                color: '#9E9E9E',
-                                fontFamily : 'var(--roboto-font)',
-                                fontWeight: 300,
-                                fontSize: '1.2rem'
-                            },
-                            actionsCellStyle:{
-                                paddingRight : "3rem"
-                            }
-                        }}
+  const columns = [
+    {
+      title: "Nom Complet",
+      field: "name_familyName",
+      render: (rowData) => (
+        <div className="roboto-500">
+          {rowData.familyName} {rowData.name}
+        </div>
+      ),
+    },
+    {
+      title: "Adresse Email",
+      field: "email",
+      render: (rowData) => <div className="roboto-500">{rowData.email}</div>,
+    },
+    {
+      title: "Numéro de téléphone",
+      field: "phoneNumber",
+      render: (rowData) => (
+        <div className="roboto-500">{rowData.phone_number}</div>
+      ),
+    },
+    {
+      title: "Profile",
+      field: "profile",
+      render: (rowData) => (
+        <img
+          className="hoverable"
+          onClick={() => {
+            /**/
+            setAmProfileDialogOpenStatus(true);
+            dispatch(fetchgetAM(rowData.agent_id));
+          }}
+          style={{ height: "40px", width: "40px" }}
+          src={profileIcon}
+        />
+      ),
+    },
+    {
+      title: "Delete",
+      field: "Delete",
+      render: (rowData) => (
+        <img
+          className="hoverable"
+          onClick={() => {
+            /**/
+            setConfirmDialogOpenStatus(true);
+            setAmEmail(rowData.email);
+          }}
+          style={{ height: "40px", width: "40px" }}
+          src={RemoveIcon}
+        />
+      ),
+    },
+  ];
 
-                    />
-                    <ProfileDialog isOpen={isAmProfileDialogopen} setOpen={setAmProfileDialogOpenStatus}/>
-                    <ConfirmDialog isOpen={isConfirmDialogopen} setOpen={setConfirmDialogOpenStatus} amEmail={amEmail}/>
-                    <SignUpDialog isOpen={isSignUpDialogopen} setOpen={setSignUpDialogOpenStatus} />
-                </div>
-            )
+  const actions = [
+    {
+      icon: () => (
+        <img style={{ height: "30px", width: "30px" }} src={AddIcon} />
+      ),
+      tooltip: "Ajouter un agent",
+      position: "toolbar",
+      isFreeAction: true,
+      onClick: (event) => {
+        setSignUpDialogOpenStatus(true);
+      },
+    },
+  ];
+  const data = [
+    {
+      name: "Mehmet",
+      surname: "Baran",
+      birthYear: 1987,
+      birthCity: 63,
+      imageUrl: "https://avatars0.githubusercontent.com/u/7895451?s=460&v=4",
+    },
+    {
+      name: "Zerya Betül",
+      surname: "Baran",
+      birthYear: 2017,
+      birthCity: 34,
+      imageUrl: "https://avatars0.githubusercontent.com/u/7895451?s=460&v=4",
+    },
+  ];
 
-    );
+  return (
+    <div className="bg-white" style={{ height: 400, width: "100%" }}>
+      <MaterialTable
+        style={{ borderRadius: "25px" }}
+        icons={tableIcons}
+        localization={tableLang}
+        title="Gestions des comptes des agents de maintenance"
+        columns={columns}
+        data={data}
+        actions={actions}
+        options={{
+          search: false,
+          selection: true,
+          sorting: true,
+          //filtering:true,
+          showSelectAllCheckbox: true,
+          showTextRowsSelected: true,
+          actionsColumnIndex: -1,
+          headerStyle: {
+            color: "#9E9E9E",
+            fontFamily: "var(--roboto-font)",
+            fontWeight: 300,
+            fontSize: "1.2rem",
+          },
+          actionsCellStyle: {
+            paddingRight: "3rem",
+          },
+        }}
+      />
+      <ProfileDialog
+        isOpen={isAmProfileDialogopen}
+        setOpen={setAmProfileDialogOpenStatus}
+      />
+      <ConfirmDialog
+        isOpen={isConfirmDialogopen}
+        setOpen={setConfirmDialogOpenStatus}
+        amEmail={amEmail}
+      />
+      <SignUpDialog
+        isOpen={isSignUpDialogopen}
+        setOpen={setSignUpDialogOpenStatus}
+      />
+    </div>
+  );
 }
--- src/Routes/Ams/Compounents/ProfileDialog.Compounent.js
+++ src/Routes/Ams/Compounents/ProfileDialog.Compounent.js
@@ -1,36 +1,99 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import {FormGroup, Input, Label} from "reactstrap";
-import './styles.css'
 import Tasks from "./Tasks.Compounent";
 
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { FormGroup, Input, Label } from "reactstrap";
+import "./styles.css";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchRejectLocataire } from "../../../redux/actions/actions";
+import ProfileView from "./ProfileView.Compoument";
+import CustomizedTables from "./DifferentsVehicules.Compounent";
 
-export default function ProfileDialog({isOpen , setOpen }) {
+export default function ProfileDialog({ isOpen, setOpen, amId }) {
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  // replace with request
+  const Userinfos = {
+    nom: "nom",
+    prenom: "prenom",
+    mail: "mail@gmail.com",
+    tel: "0721548631",
+    mdp: "Mot de passe",
+    libre: true,
+  };
 
-    return (
-        <div>
-            <Dialog className="p-2" open={isOpen} onClose={handleClose}>
-                <DialogTitle style={{fontSize : "2rem"}} className="lora-700 gacela-orange">Détails</DialogTitle>
-                <DialogContent>
-                    <DialogContentText style={{fontSize : "1.4rem"}} className="lora-400 gacela-black21 mb-lg-3">
-                        datails
-                    </DialogContentText>
-                    <Tasks/>
-                </DialogContent>
-            </Dialog>
-        </div>
-    );
+  // replace with request
+  const CarsData = [
+    {
+      id: "V-12D54",
+      etat: "réservé",
+    },
+    {
+      id: "V-1FF54",
+      etat: "diponible",
+    },
+    {
+      id: "V-1R454",
+      etat: "réservé",
+    },
+    {
+      id: "V-1S024",
+      etat: "diponible",
+    },
+    {
+      id: "V-12D54",
+      etat: "réservé",
+    },
+    {
+      id: "V-1FF54",
+      etat: "diponible",
+    },
+    {
+      id: "V-1R454",
+      etat: "réservé",
+    },
+    {
+      id: "V-1S024",
+      etat: "diponible",
+    },
+    {
+      id: "V-12D54",
+      etat: "réservé",
+    },
+    {
+      id: "V-1FF54",
+      etat: "diponible",
+    },
+    {
+      id: "V-1R454",
+      etat: "réservé",
+    },
+    {
+      id: "V-1S024",
+      etat: "diponible",
+    },
+  ];
+
+  return (
+    <div>
+      <Dialog className="p-2" open={isOpen} onClose={handleClose}>
+        <ProfileView Userinfos={Userinfos} />
+
+        <CustomizedTables CarsData={CarsData} />
+        {/*  <Tasks /> */}
+      </Dialog>
+    </div>
+  );
 }

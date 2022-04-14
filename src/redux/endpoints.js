12573fa2521b99b91c508edd4576980815ac56f6
namgalff <96527743+namgalff@users.noreply.github.com>
2022-04-14 12:34:52 +0200

    fixing gacela-token issue + get demande locataire response change

--- src/redux/endpoints.js
+++ src/redux/endpoints.js
@@ -4,7 +4,7 @@ module.exports = {
   ENDPOINT_POST_POST: `${baseUrlTest}/posts`,
   ENDPOINT_POST_LOGIN: `${baseUrlTest}/api/web_login`,
   ENDPOINT_POST_RESET_PASSWORD: `${baseUrlTest}/api/web_passwordReset/admin`,
-  ENDPOINT_GET_LOCATAIRES_REQUESTS: `${baseUrlTest}/api/locataire/non_validated`,
+  ENDPOINT_GET_LOCATAIRES_REQUESTS: `${baseUrlTest}/api/locataire/waiting`,
   ENDPOINT_POST_ACCEPT_LOCATAIRE: `${baseUrlTest}/api/signup/locataire/validate`,
   ENDPOINT_POST_REJECT_LOCATAIRE: `${baseUrlTest}/api/signup/locataire/reject`,
   ENDPOINT_GET_DEVEROUILLAGE_REQUESTS: `${baseUrlTest}/api/locataire/validated`,
